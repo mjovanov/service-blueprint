b322b70e88b1f33d5cb41b292872a69eac92acda
Mihajlo Jovanovic <mihajlo@Mihajlos-MacBook.local>
2016-11-11 09:58:08 -0500

    Initial Commit

---
+++ src/main/java/io/mihajlo/App.java
@@ -0,0 +1,13 @@
+package io.mihajlo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
